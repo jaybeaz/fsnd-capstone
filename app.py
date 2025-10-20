96fb2b947b0f323a03f79033541115bd1aa4c56f
Jason B <jaybeaz@gmail.com>
2025-10-19 17:15:51 -0700

    Updates models and adds actors and movies enpoints

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, abort, jsonify
+from models import setup_db, Actors, Movies, db
 from flask_cors import CORS
+
+#from .database.models import 
+#from .auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
+    CORS(app, origins=['*']) #specifying origins arg here makes explicit but not necessary
 
     @app.route('/')
     def get_greeting():
         excited = os.environ['EXCITED']
         greeting = "Hello" 
+        print(excited)
         if excited == 'true': 
             greeting = greeting + "!!!!! You are doing great in this Udacity project."
         return greeting
@@ -23,11 +27,176 @@ def create_app(test_config=None):
     
     @app.route('/lebowski')
     def lebowski():
-        return "I'm not Mr Lebowski, I'm The Dude, so that's what you call me."
+        return '''Let me explain something to you. Um, I am not "Mr. Lebowski". You're Mr. Lebowski. I'm the Dude. So that's what you call me. You know, that or, uh, His Dudeness, or uh, Duder, or El Duderino if you're not into the whole brevity thing.'''
 
     return app
 
 app = create_app()
 
+@app.route('/actors', methods=["GET"])
+def get_actors():
+    actors = Actors.query.order_by(Actors.id).all()
+    formatted_actors = [actor.format() for actor in actors]
+    return jsonify(
+        {
+            "success": True,
+            "actors": formatted_actors,
+            "total_actors": len(formatted_actors)
+        }
+    )
+
+@app.route('/movies', methods=["GET"])
+def get_movies():
+    movies = Movies.query.order_by(Movies.id).all()
+    formatted_movies = [movie.format() for movie in movies]
+    return jsonify(
+        {
+            "success": True,
+            "movies": formatted_movies,
+            "total_movies": len(formatted_movies)
+        }
+    )
+
+@app.route('/actors', methods=["POST"])
+#@requires_auth('create:actors')
+def create_actors():
+    body = request.get_json()
+    actor_name = body.get("name", None)
+    actor_age = body.get("age", None)
+    actor_gender = body.get("gender", None)
+    try:
+        actor = Actors(name=actor_name, age=actor_age, gender=actor_gender)
+        actor.insert()
+
+        actors = Actors.query.order_by(Actors.id).all()
+        formatted_actors = [a.format() for a in actors]
+        if len(formatted_actors) == 0:
+            abort(404)
+        return jsonify(
+            {
+                "success": True,
+                "created": actor.id,
+                "actors": formatted_actors,
+                "total_actors": len(formatted_actors)
+            }
+        )
+    except Exception as e:
+        abort(422)
+
+@app.route('/movies', methods=["POST"])
+#@requires_auth('create:movies')
+def create_movies():
+    body = request.get_json()
+    movie_title = body.get("title", None)
+    movie_release_date = body.get("release_date", None)
+    try:
+        movie = Movies(title=movie_title, release_date=movie_release_date)
+        movie.insert()
+
+        movies = Movies.query.order_by(Movies.id).all()
+        formatted_movies = [m.format() for m in movies]
+        if len(formatted_movies) == 0:
+            abort(404)
+        return jsonify(
+            {
+                "success": True,
+                "created": movie.id,
+                "movies": formatted_movies,
+                "total_movies": len(formatted_movies)
+            }
+        )
+    except Exception as e:
+        abort(422)
+
+@app.route('/actors/<int:actor_id>', methods=["PATCH"])
+#@requires_auth('patch:actors')
+def update_actors(actor_id):
+    body = request.get_json()
+    if not body:
+        abort(400)
+    actor = Actors.query.filter(Actors.id == actor_id).one_or_none()
+    if actor is None:
+        abort(404)
+    
+    new_name = body.get("name", None)
+    new_age = body.get("age", None)
+    new_gender = body.get("gender", None)
+
+    if not new_age and not new_name and not new_gender:
+        abort(400)
+    
+    try:
+        if new_name:
+            actor.name = new_name
+        if new_age:
+            actor.age = new_age
+        if new_gender:
+            actor.gender = new_gender
+        actor.update()
+        return jsonify({
+            "success": True,
+            "actor": actor.id
+        }, 200)
+    except Exception as e:
+        abort(422)
+
+@app.route('/movies/<int:movie_id>', methods=["PATCH"])
+#@requires_auth('patch:movies')
+def update_movies(movie_id):
+    body = request.get_json()
+    if not body:
+        abort(400)
+    movie = Movies.query.filter(Movies.id == movie_id).one_or_none()
+    if movie is None:
+        abort(404)
+    movie_title = body.get("title", None)
+    release_date = body.get("release_date", None)
+    if not movie_title and not release_date:
+        abort(400)
+
+    try:
+        if movie_title:
+            movie.title = movie_title
+        if release_date:
+            movie.release_date = release_date
+        movie.update()
+        return jsonify({
+            "success": True,
+            "movie": movie.id
+        })
+    except Exception as e:
+        abort(422)
+
+@app.route('/actors/<int:actor_id>', methods=["DELETE"])
+#@requires_auth('delete:actors')
+def delete_actors(actor_id):
+    try:
+        actor = Actors.query.filter(Actors.id == actor_id).one_or_none()
+        if actor is None:
+            abort(404)
+        actor.delete()
+        return jsonify({
+            "success": True,
+            "delete": actor_id
+        }, 200)
+    except Exception as e:
+        abort(422)
+
+@app.route('/movies/<int:movie_id>', methods=["DELETE"])
+#@requires_auth('delete:movies')
+def delete_movies(movie_id):
+    try:
+        movie = Movies.query.filter(Movies.id == movie_id).one_or_none()
+        if movie is None:
+            abort(404)
+        movie.delete()
+        return jsonify({
+            "success": True,
+            "movie": movie_id
+        }, 200)
+    except Exception as e:
+        abort(422)
+
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(port=3000)
--- models.py
+++ models.py
@@ -23,7 +23,6 @@ def setup_db(app, database_path=database_path):
 
 '''
 Person
-Have title and release year
 '''
 class Person(db.Model):  
   __tablename__ = 'People'
@@ -41,3 +40,64 @@ class Person(db.Model):
       'id': self.id,
       'name': self.name,
       'catchphrase': self.catchphrase}
+  
+class Movies(db.Model):
+  __tablename__ = 'Movies'
+
+  id = Column(db.Integer, primary_key=True)
+  title = Column(db.String(120))
+  release_date = db.Column(db.Date)
+
+  def __init__(self, title, release_date):
+    self.title = title
+    self.release_date = release_date
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+      db.session.commit()
+
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit() 
+
+  def format(self):
+    return {
+      'movie': self.id, 
+      'title': self.title, 
+      'release_date': self.release_date
+      }
+
+class Actors(db.Model):
+  __tablename__ = 'Actors'
+
+  id = Column(db.Integer, primary_key=True)
+  name = Column(db.String(120))
+  age = db.Column(db.Integer)
+  gender = db.Column(db.String(20))
+
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.gender = gender
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+      db.session.commit()
+
+  def delete(self):
+      db.session.delete(self)
+      db.session.commit()  
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name, 
+      'age': self.age,
+      'gender': self.gender
+      }
